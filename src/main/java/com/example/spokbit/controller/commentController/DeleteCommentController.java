8429552d7f4549290da48f6977a58c0defd0e8cc
richard <spok.com>
2023-09-06 16:48:03 -0600

    feat(backend): add class DeleteCommentController

---
+++ src/main/java/com/example/spokbit/controller/commentController/DeleteCommentController.java
@@ -0,0 +1,28 @@
+package com.example.spokbit.controller.commentController;
+
+import com.example.spokbit.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DeleteCommentController {
+    private final CommentRepository commentRepository;
+    private final DeleteComment deleteComment;
+
+    @Autowired
+    public DeleteCommentController(CommentRepository commentRepository, DeleteComment deleteComment) {
+        this.commentRepository = commentRepository;
+        this.deleteComment = deleteComment;
+    }
+
+    @GetMapping(value = "comments/{long_Id_Topic_Comments}")
+    public ResponseEntity<Void> deleteComment(@PathVariable("long_Id_Topic_Comments") Long id) {
+        deleteComment.delete(id);
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
