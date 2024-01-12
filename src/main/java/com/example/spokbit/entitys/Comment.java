94bdb492932b85989c420580db41767aa0063438
richard <spok.com>
2024-01-12 16:36:59 -0600

    se refactorizo la base de datos el tipo comment en la clase Comment de varchar a text

--- src/main/java/com/example/spokbit/entitys/Comment.java
+++ src/main/java/com/example/spokbit/entitys/Comment.java
@@ -17,7 +17,8 @@ public class Comment {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "COMMENTS")
+    @Lob
+    @Column(name = "COMMENTS", columnDefinition = "TEXT")
     private String comment;
 
     @ManyToOne
