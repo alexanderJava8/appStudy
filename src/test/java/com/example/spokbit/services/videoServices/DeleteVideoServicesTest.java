d50db87f53aab5caffbd14fcbf0c6ad7abdbc27a
richard <spok.com>
2024-03-12 11:22:04 -0600

    feature/delete videos

--- src/test/java/com/example/spokbit/services/videoServices/DeleteVideoServicesTest.java
+++ src/test/java/com/example/spokbit/services/videoServices/DeleteVideoServicesTest.java
@@ -1,6 +1,7 @@
 package com.example.spokbit.services.videoServices;
 
 import com.example.spokbit.entitys.Video;
+import com.example.spokbit.exception.exceptionVideo.VideoNotFoundException;
 import com.example.spokbit.repository.VideoRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -8,7 +9,10 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class DeleteVideoServicesTest {
     @Mock
@@ -24,6 +28,30 @@ class DeleteVideoServicesTest {
 
     @Test
     void deleteVideo() {
+        //arrange
+        Video video = new Video(1L,
+                "https://www.youtube.com/watch?v=Z3L5M7GHAkw");
+        when(videoRepository.findById(1L))
+                .thenReturn(Optional.of(video));
+        //act
+        deleteVideoServices.deleteVideo(video.getId());
+        //assert
 
+        verify(videoRepository, times(1))
+                .deleteById(video.getId());
+    }
+
+    @Test
+    void deleteVideo_nonExistingVideo() {
+        //arrange
+        Video video = new Video(3L,
+                "https://www.youtube.com/watch?v=Z3L5M7GHAkw");
+        when(videoRepository.findById(video.getId()))
+                .thenReturn(Optional.empty());
+
+        //assert
+        assertThrows(VideoNotFoundException.class, () -> {
+            deleteVideoServices.deleteVideo(3L);
+        });
     }
 }
